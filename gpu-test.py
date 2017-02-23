6ca597bc9eab961f4835ed80896790608924f0b8
James Hong <jamesh93@stanford.edu>
2017-02-22 18:52:44 -0800

    Add missing print statement

--- gpu-test.py
+++ gpu-test.py
@@ -8,5 +8,5 @@ def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
-get_available_gpus()
+print get_available_gpus()
 
